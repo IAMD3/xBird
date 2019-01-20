53e8fdaea3ad57c1c17dd14ec5d1c530d1bc9a40
tangyukai <tangy22@uni.coventry.ac.uk>
2019-01-21 01:02:09 +0800

    clean

---
+++ src/main/java/core/XBSocketServlet.java
@@ -0,0 +1,22 @@
+package core;
+
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+import javax.servlet.annotation.WebServlet;
+/**
+ * @Created by: Yukai
+ * @Date: 2019/1/21 0:12
+ * @Description : choose either XBHandler/XBSocketServlet
+ */
+@SuppressWarnings("serial")
+@WebServlet(name = "MyEcho WebSocket Servlet", urlPatterns = { "/hello" })
+public class XBSocketServlet extends WebSocketServlet {
+
+    @Override
+    public void configure(WebSocketServletFactory webSocketServletFactory) {
+        webSocketServletFactory.getPolicy().setIdleTimeout(10L * 60L * 1000L);
+        webSocketServletFactory.getPolicy().setAsyncWriteTimeout(10L * 1000L);
+        /* set webSocketCreator xxDD */
+        webSocketServletFactory.setCreator(new CoreCreator());
+    }
+}
